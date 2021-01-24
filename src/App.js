51a999ead27fd9f4dff49afe0cf298dffad7ec82
sorashin <naka52shin@gmail.com>
2021-01-24 23:04:23 +0900

    re arranged models

--- src/App.js
+++ src/App.js
@@ -9,9 +9,10 @@ import Text from './helpers/Text'
 import useModel from './helpers/useModel'
 import useYScroll from './helpers/useYScroll'
 import './styles.css'
+import { Controls, useControl, withControls } from 'react-three-gui'
 
 const material = { transparent: true, roughness: 0.8, fog: true, shininess: 0, flatShading: false }
-
+const MyCanvas = withControls(Canvas);
 
 const Stage = ({color, ...props})=>{
   const [geometries, center] = useModel('/stage.glb')
@@ -20,18 +21,38 @@ const Stage = ({color, ...props})=>{
   const hover = (e) => e.stopPropagation() && set(true)
   const unhover = () => set(false)
   const { scale } = useSpring({ scale: hovered ? 1.2 : 1, config: config.stiff })
-  console.log(geometries)
+  
+  
   
   return (
-    <a.group {...props} onPointerOver={hover} onPointerOut={unhover} scale={scale.to((s) => [s, s, 1])}>
+    <a.group {...props} onPointerOver={hover} onPointerOut={unhover} scale={scale.to((s) => [s, s, 1])} >
       {geometries.map((geom) => (
-        <mesh key={geom.uuid} position={center} geometry={geom} castShadow receiveShadow>
+        <mesh key={geom.uuid} position={center} geometry={geom} castShadow receiveShadow rotation={[-Math.PI/2, Math.PI, Math.PI/2]}>
           <meshStandardMaterial {...material} roughness={1} shininess={0} color={color} />
         </mesh>
       ))}
     </a.group>
   )
 }
+
+const Shoes = ({ color, ...props }) => {
+  const [geometries, center] = useModel('/shoes.glb')
+  const [hovered, set] = useState(false)
+  const hover = (e) => e.stopPropagation() && set(true)
+  const unhover = () => set(false)
+  const { scale } = useSpring({ scale: hovered ? 1.2 : 1, config: config.stiff })
+  return (
+    <a.group {...props} onPointerOver={hover} onPointerOut={unhover} scale={scale.to((s) => [s, s, 1])}>
+      {geometries.map((geom) => (
+        <mesh key={geom.uuid} position={center} geometry={geom} castShadow receiveShadow rotation={[-Math.PI/2, Math.PI, 0]}>  
+          <meshPhysicalMaterial {...material} roughness={1} shininess={0} color={color} />
+        </mesh>
+      ))}
+    </a.group>
+  )
+}
+
+
 const Seat = ({ color, ...props }) => {
   const [geometries, center] = useModel('/seat.glb')
   const [hovered, set] = useState(false)
@@ -41,7 +62,7 @@ const Seat = ({ color, ...props }) => {
   return (
     <a.group {...props} onPointerOver={hover} onPointerOut={unhover} scale={scale.to((s) => [s, s, 1])}>
       {geometries.map((geom) => (
-        <mesh key={geom.uuid} position={center} geometry={geom} castShadow receiveShadow>
+        <mesh key={geom.uuid} position={center} geometry={geom} castShadow receiveShadow> 
           <meshPhysicalMaterial {...material} roughness={1} shininess={0} color={color} />
         </mesh>
       ))}
@@ -51,10 +72,10 @@ const Seat = ({ color, ...props }) => {
 
 const Quarter = ({ color, ...props }) => (
   <group {...props}>
-    <Seat color={color} position={[-0.35, 0, 0.7]} />
-    <Seat color={color} position={[0.35, 0, 0.7]} />
-    <Seat color={color} position={[-0.35, 0, -0.7]} rotation={[0, Math.PI, 0]} />
-    <Seat color={color} position={[0.35, 0, -0.7]} rotation={[0, Math.PI, 0]} />
+    {/* <Shoes color={color} position={[-0.35, -2, 0.7]} />
+    <Shoes color={color} position={[0.35, -2, 0.7]} /> */}
+    <Shoes color={color} position={[-0.35, -2, -0.7]} rotation={[0, Math.PI, 0]} />
+    <Shoes color={color} position={[0.35, -2, -0.7]} rotation={[0, Math.PI, 0]} />
   </group>
 )
 
@@ -64,8 +85,8 @@ const Row = ({ color, ...props }) => (
     <Text color="gray" size={0.15} position={[2.2, 2, 10]} rotation={[-Math.PI / 2, 0, 0]} children="2" />
     <Text color="gray" size={0.15} position={[-2.2, 2, 10.8]} rotation={[-Math.PI / 2, 0, 0]} children="1" />
     <Text color="gray" size={0.15} position={[-2.2, 2, 10]} rotation={[-Math.PI / 2, 0, 0]} children="2" />
-    <Quarter color={color} position={[-1.2, -0.45, 9.75]} />
-    <Quarter color={color} position={[1.2, -0.45, 9.75]} />
+    <Quarter color={color} position={[-1.6, -0.45, 9.75]} />
+    <Quarter color={color} position={[1.6, -0.45, 9.75]} />
   </group>
 )
 
@@ -76,30 +97,32 @@ const Cabin = ({ color = 'white', seatColor = 'white', name, ...props }) => {
       <Text centerX={false} color="lightcoral" size={0.6} position={[2.6, 2, 10.6]} rotation={[-Math.PI / 2, 0, 0]}>
         {name}
       </Text>
-      <group position={center}>
+      {/* <group position={center}>
         {geometries.map((geom, index) => (
           <mesh key={geom.uuid} geometry={geom} castShadow receiveShadow>
             <meshPhysicalMaterial {...material} color={index === 0 ? color : 'indianred'} opacity={index === 0 ? 1 : 0.2} />
           </mesh>
         ))}
-      </group>
+      </group> */}
       <Row color={seatColor} />
-      <Row color={seatColor} position={[0, 0, -1.9]} />
+      {/* <Row color={seatColor} position={[0, 0, -1.9]} />
       <Row color={seatColor} position={[0, 0, -6.6]} />
       <Row color={seatColor} position={[0, 0, -8.5]} />
       <Row color={seatColor} position={[0, 0, -11]} />
       <Row color={seatColor} position={[0, 0, -12.9]} />
       <Row color={seatColor} position={[0, 0, -17.6]} />
-      <Row color={seatColor} position={[0, 0, -19.5]} />
+      <Row color={seatColor} position={[0, 0, -19.5]} /> */}
     </group>
   )
 }
 
 export default function App() {
-  const [y] = useYScroll([-100, 2400], { domTarget: window })
+  const [y] = useYScroll([-100, 240], { domTarget: window })
+  // const positionX = useControl('Position', { type: 'number', distance: 1 });
+
   return (
-    <>
-      <Canvas concurrent invalidateFrameloop shadowMap camera={{ position: [0, 5, 10], fov: 65, near: 2, far: 60 }} gl={{ alpha: false }}>
+    <Controls.Provider>
+      <MyCanvas concurrent invalidateFrameloop shadowMap camera={{ position: [0, 5, 10], fov: 65, near: 2, far: 60 }} gl={{ alpha: false }}>
         <color attach="background" args={['white']} />
         <fog attach="fog" args={['white', 20, 40]} />
         <ambientLight intensity={0.4} />
@@ -116,13 +139,13 @@ export default function App() {
           <meshBasicMaterial color="lightcoral" fog={false} transparent opacity={0.4} />
         </mesh>
         <Suspense fallback={null}>
-          <a.group position-z={y.to((y) => (y / 500) * 25)}>
-            <Cabin color="white" seatColor="lightskyblue" name="1A" position={[0, 0, -6]} />
-            <Cabin color="lightgray" seatColor="gray" name="2B" position={[0, 0, -32]} />
-            <Cabin color="white" seatColor="lightskyblue" name="3A" position={[0, 0, -58]} />
-            <Cabin color="lightgray" seatColor="gray" name="4B" position={[0, 0, -84]} />
-            <Cabin color="#676767" seatColor="sandybrown" name="5B" position={[0, 0, -110]} />
-            <Stage color="lightskyblue" position={[0, 0, -100]} rotation={[0, Math.PI/2, 0]}/>
+          <a.group position-z= {y.to((y) => (y / 500) * 25)}>
+            <Cabin color="white" seatColor="lightskyblue" name="1A" position={[0, 0, 0]} />
+            <Cabin color="lightgray" seatColor="gray" name="2B" position={[0, 0, -3]} />
+            <Cabin color="white" seatColor="lightskyblue" name="3A" position={[0, 0, -6]} />
+            <Cabin color="lightgray" seatColor="gray" name="4B" position={[0, 0, -9]} />
+            <Cabin color="#676767" seatColor="sandybrown" name="5B" position={[0, 0, -12]} />
+            <Stage color="lightskyblue" position={[0, 0, -8]} />
           </a.group>
           
           <EffectComposer multisampling={0}>
@@ -139,8 +162,9 @@ export default function App() {
             <SMAA />
           </EffectComposer>
         </Suspense>
-      </Canvas>
-      <aDom.div className="bar" style={{ height: y.interpolate([-100, 2400], ['0%', '100%']) }} />
-    </>
+      </MyCanvas>
+      <Controls />
+      <aDom.div className="bar" style={{ height: y.interpolate([-100, 240], ['0%', '100%']) }} />
+    </Controls.Provider>
   )
 }
